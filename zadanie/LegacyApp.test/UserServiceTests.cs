cbb5cbe6f6046533d5ad99e7ce33428a5e1bd67a
Piotr Ryzak <s28771@pjwstk.edu.pl>
2024-03-26 11:12:45 +0100

    comit

---
+++ zadanie/LegacyApp.test/UserServiceTests.cs
@@ -0,0 +1,42 @@
+namespace LegacyApp.test;
+public class UserServiceTests
+{
+    [Fact]
+    public void AddUser_ReturnsFalseWhenFirstNameIsEmpty()
+    {
+// Arrange
+        var userService = new UserService();
+// Act
+        var result = userService.AddUser(
+            null,
+            "Kowalski",
+            "kowalski@kowal.com",
+            DateTime.Parse("2000-01-01"),
+            1
+        );
+// Assert
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void AddUser_ThrowsExceptionWhenClientDoesNotExist()
+    {
+// Arrange
+        var userService = new UserService();
+// Act
+        Action action = () =>
+        {
+            userService.AddUser(
+                "Jan",
+                "Kowalski",
+                "kowalski@kowal.com",
+                DateTime.Parse("2000-01-01"),
+                100
+            );
+        };
+        
+        
+// Assert
+        Assert.Throws<ArgumentException>(action);
+    }
+}
